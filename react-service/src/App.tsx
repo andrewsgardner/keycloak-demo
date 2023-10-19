8889f4098d6d7f46d33d7c354c40f735fd50943b
Andrew S. Gardner <asg@andrewsgardner.com>
2023-10-18 20:03:28 -0400

    Add Keycloak

--- react-service/src/App.tsx
+++ react-service/src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
+import Keycloak, { KeycloakInitOptions } from 'keycloak-js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 
 import './App.scss';
-import routes from './routes';
 import theme from './theme';
+import routes from './routes';
 import Toolbar from './components/Toolbar/Toolbar';
 
 const router = createBrowserRouter(routes);
 
 const App = () => {
+
+  const keycloak = new Keycloak({
+    url: 'http://localhost:8080/',
+    realm: 'demo',
+    clientId: 'react-client',
+  });
+
+  const keycloakProviderInitConfig: KeycloakInitOptions = {
+    onLoad: 'login-required',
+  }
+  
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Toolbar />
-      <main>
-        <RouterProvider router={router} fallbackElement={<CircularProgress />} />
-      </main>
-    </ThemeProvider>
+    <ReactKeycloakProvider 
+      authClient={keycloak}
+      initOptions={keycloakProviderInitConfig}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Toolbar />
+        <main>
+          <RouterProvider router={router} fallbackElement={<CircularProgress />} />
+        </main>
+      </ThemeProvider>
+    </ReactKeycloakProvider>
   );
 }
 
