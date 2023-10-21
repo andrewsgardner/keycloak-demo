bb3f55217f3f9678c08555f27bc29b5e20eb270c
Andrew S. Gardner <asg@andrewsgardner.com>
2023-10-21 16:14:54 -0400

    Remove unused import

--- react-service/src/providers/routes.tsx
+++ react-service/src/providers/routes.tsx
@@ -1,6 +1,5 @@
 import { RouteObject } from "react-router";
 import TokenViz from "../components/TokenViz/TokenViz";
-import { useKeycloak } from "@react-keycloak/web";
 
 const routes: RouteObject[] = [
     {
