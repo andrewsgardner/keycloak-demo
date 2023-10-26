dc78d4ef54c87545cdbb0e4f47a41c0d8afe9dd9
Andrew S. Gardner <asg@andrewsgardner.com>
2023-10-25 20:57:24 -0400

    Add kcUrl env variable

---
+++ angular-service/src/assets/env.template.js
@@ -0,0 +1,6 @@
+(function (window) {
+    window['env'] = window['env'] || {};
+
+    // Environment variables
+    window['env']['kcUrl'] = `${KC_URL}`;
+})(this);
--- angular-service/src/environments/environment.ts
+++ angular-service/src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { KeycloakConfig } from "keycloak-js";
 
 const keycloakConfig: KeycloakConfig = {
-    url: 'http://localhost:8080/',
+    url: (window as any)['env']['kcUrl'],
     realm: 'demo',
     clientId: 'angular-client'
   };
