a946dfb0499dff22b146d05dbe9763142d17c3eb
Andrew S. Gardner <asg@andrewsgardner.com>
2023-11-07 21:01:48 -0500

    Add environment variables

--- react-service/src/index.tsx
+++ react-service/src/index.tsx
@@ -9,15 +9,15 @@ import { KeycloakConfig } from 'keycloak-js';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const keycloakConfig: KeycloakConfig = {
-    url: 'http://localhost:8080',
+    url: process.env.REACT_APP_KC_URL || 'http://localhost:8080',
     realm: 'demo',
     clientId: 'react-client',
 };
 const oidcConfig: AuthProviderProps = {
     authority: `${keycloakConfig.url}/realms/${keycloakConfig.realm}`,
     client_id: keycloakConfig.clientId,
-    redirect_uri: 'http://localhost:3000',
-    post_logout_redirect_uri: 'http://localhost:3000',
+    redirect_uri: process.env.REACT_APP_CLIENT_URL || 'http://localhost:3000',
+    post_logout_redirect_uri: process.env.REACT_APP_CLIENT_URL || 'http://localhost:3000',
     revokeTokensOnSignout: true,
 };
 
