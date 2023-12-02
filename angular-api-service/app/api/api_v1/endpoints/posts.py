a2f2cb37c4a90d28154e08873cec306e399fb586
Andrew S. Gardner <asg@andrewsgardner.com>
2023-12-02 15:40:30 -0500

    Add get by id and post methods

--- angular-api-service/app/api/api_v1/endpoints/posts.py
+++ angular-api-service/app/api/api_v1/endpoints/posts.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 from app.api.deps import SessionDep
-from app.models import Post, PostOut
+from app.models import Post, PostOut, PostCreate
+from uuid import UUID
 
 router = APIRouter()
 
@@ -12,3 +13,26 @@ def read_posts(session: SessionDep, skip: int = 0, limit: int = 100) -> list[Pos
     """
     statement = select(Post).offset(skip).limit(limit)
     return session.exec(statement).all()
+
+@router.get("/{id}")
+def read_post(session: SessionDep, id: UUID) -> PostOut:
+    """
+    Retrieve post by id.
+    """
+    post = session.get(Post, id)
+
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    
+    return post
+
+@router.post("/")
+def create_post(*, session: SessionDep, post_in: PostCreate) -> PostOut:
+    """
+    Create a post.
+    """
+    post = Post.from_orm(post_in)
+    session.add(post)
+    session.commit()
+    session.refresh(post)
+    return post
