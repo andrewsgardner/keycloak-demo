a519a425eb36a2ea11b7b0eff6ecdb7ea9007b84
Andrew S. Gardner <asg@andrewsgardner.com>
2023-12-25 15:41:19 -0500

    Remove login route

--- blog-api-service/app/api/api_v1/api.py
+++ blog-api-service/app/api/api_v1/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import login, users, posts
+from app.api.api_v1.endpoints import users, posts
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
--- blog-api-service/app/api/api_v1/endpoints/login.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-# Add login endpoints
