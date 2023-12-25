cf1f1a5959cd40c1a1ce5f48e91651fb13f6ac53
Andrew S. Gardner <asg@andrewsgardner.com>
2023-12-25 15:36:18 -0500

    Add method to get user by id

--- blog-api-service/app/api/api_v1/endpoints/users.py
+++ blog-api-service/app/api/api_v1/endpoints/users.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from sqlmodel import select
 from app.api.deps import SessionDep, get_user_info
 from app.models import TokenUser, User, UserOut
+from uuid import UUID
 
 router = APIRouter()
 
@@ -12,3 +13,15 @@ def read_users(session: SessionDep, skip: int = 0, limit: int = 100, token_user:
     """
     statement = select(User).offset(skip).limit(limit)
     return session.exec(statement).all()
+
+@router.get("/{id}")
+def read_user(session: SessionDep, id: UUID, token_user: TokenUser = Depends(get_user_info)) -> UserOut:
+    """
+    Retrieve user by id.
+    """
+    user = session.get(User, id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user
