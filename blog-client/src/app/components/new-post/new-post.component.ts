6ac15a67cd31f5fce5576413b98c2121f4c8c4cf
Andrew S. Gardner <asg@andrewsgardner.com>
2024-01-06 15:53:27 -0500

    Add new post form

--- blog-client/src/app/components/new-post/new-post.component.ts
+++ blog-client/src/app/components/new-post/new-post.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { IUser } from 'src/app/models/user.interface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-new-post',
@@ -7,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  public authUser$: Observable<IUser>;
+  public newPostValue: FormControl<string | null> = new FormControl<string | null>('', [Validators.required]);
+
+  constructor(
+    private userService: UserService,
+  ) {
+    this.authUser$ = this.userService.authUser$;
+  }
+
+  public getInitials(user: IUser): string {
+    if (user == null) {
+      return '';
+    }
+
+    return `${user.first_name[0]?.toUpperCase()} ${user.last_name[0]?.toUpperCase()}`;
+  }
+
+  public submitPost(): void {
+  }
 }
