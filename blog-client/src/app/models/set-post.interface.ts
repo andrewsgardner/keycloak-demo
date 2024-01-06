bca0dde0e895e1dd2b90d0afaa57b2e6ca6b945d
Andrew S. Gardner <asg@andrewsgardner.com>
2024-01-05 21:53:04 -0500

    Remove ISetPost

--- blog-client/src/app/models/set-post.interface.ts
+++
@@ -1,7 +0,0 @@
-import { SetPostAction } from "../enums/set-post-action.enum";
-import { IPost } from "./post.interface";
-
-export interface ISetPost {
-    action: SetPostAction;
-    payload: IPost;
-}
