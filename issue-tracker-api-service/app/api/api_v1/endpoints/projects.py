230ee230fb502d6f489e6df79b3f4da5e74ad2a0
Andrew S. Gardner <asg@andrewsgardner.com>
2024-02-23 19:09:48 -0500

    Add call to get project by id

--- issue-tracker-api-service/app/api/api_v1/endpoints/projects.py
+++ issue-tracker-api-service/app/api/api_v1/endpoints/projects.py
@@ -1,5 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from sqlmodel import select
+from app.models import Project, ProjectOut
+from app.api.deps import SessionDep, TokenDep
 
 router = APIRouter()
 
-# TODO: Add endpoints
+@router.get("/")
+def read_projects(session: SessionDep) -> list[ProjectOut]:
+    """
+    Retrieve all projects.
+    """
+    statement = select(Project)
+    return session.exec(statement).all()
+
+@router.get("/{id}")
+def read_projects(session: SessionDep, id: int) -> ProjectOut:
+    """
+    Retrieve project by id.
+    """
+    project = session.get(Project, id)
+
+    if not project:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project not found.")
+    
+    return project
