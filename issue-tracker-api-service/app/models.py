bdd3e68915a6aecc1d3cbbfae31c7e28af0f691c
Andrew S. Gardner <asg@andrewsgardner.com>
2024-02-26 21:58:52 -0500

    Add issue models

--- issue-tracker-api-service/app/models.py
+++ issue-tracker-api-service/app/models.py
@@ -1,8 +1,9 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, date
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
 from sqlalchemy import text
+from enum import Enum
 from typing import List, Optional, Union
 
 class TokenUser(BaseModel):
@@ -81,3 +82,70 @@ class ProjectOut(ProjectBase):
     create_date: datetime
     modified_by: Union[str, None]
     modified_date: datetime
+
+class IssueStatus(Enum):
+    OPEN = "Open"
+    ONHOLD = "On-Hold"
+    CLOSED = "Closed"
+
+class IssuePriority(Enum):
+    HIGH = "High"
+    MEDIUM = "Medium"
+    LOW = "Low"
+
+class IssueBase(SQLModel):
+    issue_summary: Optional[str]
+    issue_description: Union[str, None]
+    issue_status: IssueStatus
+    issue_priority: Union[IssuePriority, None]
+    target_resolution_date: Union[date, None]
+    actual_resolution_date: Union[date, None]
+    resolution_summary: Union[str, None]
+
+class Issue(IssueBase, table=True):
+    __tablename__: str = "issues"
+
+    id: Optional[int] = Field(
+        default=None, 
+        primary_key=True,
+        index=True,
+        nullable=False
+    )
+    created_by: Union[str, None] = Field(
+        index=True,
+    )
+    create_date: datetime = Field(
+        default_factory=datetime.utcnow,
+        nullable=False,
+        sa_column_kwargs={
+            "server_default": text("current_timestamp")
+        }
+    )
+    modified_by: Union[str, None] = Field(
+        index=True,
+    )
+    modified_date: datetime = Field(
+        default_factory=datetime.utcnow,
+        nullable=False,
+        sa_column_kwargs={
+            "server_default": text("current_timestamp"),
+            "onupdate": text("current_timestamp")
+       }
+    )
+    assigned_to: Union[str, None] = Field(
+        index=True,
+    )
+
+class IssueCreate(IssueBase):
+    created_by: str
+
+class IssueUpdate(IssueBase):
+    modified_by: str
+
+class IssueOut(IssueBase):
+    id: int
+    created_by: str
+    create_date: datetime
+    modified_by: str
+    modified_date: datetime
+    assigned_to: Union[str, None]
