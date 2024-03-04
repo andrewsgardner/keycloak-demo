55a1b451b9c72aeb1d9b42e8c6bb2c30d39ca600
Andrew S. Gardner <asg@andrewsgardner.com>
2024-03-04 18:52:26 -0500

    Add environment variables

--- issue-tracker-api-service/README.md
+++ issue-tracker-api-service/README.md
@@ -6,6 +6,20 @@ A FastAPI server for the Issue Tracker Client.
 
 - Python 3.11.2
 
+## Environment Variables
+
+| Name   | Type |
+|--------|-----|
+| SERVER_HOST | str |
+| SERVER_PORT | int |
+| POSTGRES_SERVER | str |
+| POSTGRES_USER | str |
+| POSTGRES_PASSWORD | str |
+| POSTGRES_DB | str |
+| KC_SERVER_URL | str |
+| KC_CLIENT_ID | str |
+| KC_REALM | str |
+
 ## Run Local
 
 ### Setup virtual environment
--- issue-tracker-api-service/app/core/config.py
+++ issue-tracker-api-service/app/core/config.py
@@ -1,13 +1,13 @@
 import os
-from typing import List, Any, Optional, Union
+from typing import List, Optional, Union
 from pydantic_settings import BaseSettings
 from pydantic import AnyHttpUrl, PostgresDsn, field_validator, ValidationInfo
 from app.core.tags import tags_metadata
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SERVER_HOST: str = "localhost" # os.getenv("SERVER_HOST")
-    SERVER_PORT: int = 9000
+    SERVER_HOST: str = os.getenv("SERVER_HOST")
+    SERVER_PORT: int = os.getenv("SERVER_PORT")
     PROJECT_TITLE: str = "Issue Tracker API Service"
     PROJECT_DESCRIPTION: str = "A FastAPI server for the Issue Tracker Client."
     PROJECT_VERSION: str = "1.0.0"
@@ -16,10 +16,10 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://localhost:4000"
     ]
-    POSTGRES_SERVER: str = "localhost:5432" # os.getenv("POSTGRES_SERVER") - postgres-service
-    POSTGRES_USER: str = "postgres" # os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD: str = "postgres" # os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB: str = "issue_tracker_data" #os.getenv("POSTGRES_DB")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]] = None
 
     @field_validator("SQLALCHEMY_DATABASE_URI", mode="after")
@@ -34,10 +34,8 @@ class Settings(BaseSettings):
             path=values.data.get("POSTGRES_DB")
         ).unicode_string()
     
-    KC_SERVER_URL: str = "http://localhost:8080" # os.getenv("KC_SERVER_URL")
-    KC_AUTH_URL: str = "http://localhost:8080" # os.getenv("KC_AUTH_URL")
-    KC_TOKEN_URL: str = "http://localhost:8080" # os.getenv("KC_TOKEN_URL")
-    KC_CLIENT_ID: str = "issue-tracker-api" # os.getenv("KC_CLIENT_ID")
-    KC_REALM: str = "demo" # os.getenv("KC_REALM")
+    KC_SERVER_URL: str = os.getenv("KC_SERVER_URL")
+    KC_CLIENT_ID: str = os.getenv("KC_CLIENT_ID")
+    KC_REALM: str = os.getenv("KC_REALM")
 
 settings = Settings()
