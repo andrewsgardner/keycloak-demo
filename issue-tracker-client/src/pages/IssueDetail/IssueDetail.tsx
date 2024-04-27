3532d3a92b7592b2670c9706caa9e004c3439f1d
Andrew S. Gardner <asg@andrewsgardner.com>
2024-04-26 22:10:21 -0400

    Select Assignee

--- issue-tracker-client/src/pages/IssueDetail/IssueDetail.tsx
+++ issue-tracker-client/src/pages/IssueDetail/IssueDetail.tsx
@@ -1,14 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 
 import './IssueDetail.scss';
+import SearchIcon from '@mui/icons-material/Search';
 import { AppContext } from '../../contexts/AppContext';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { Box, Theme, Typography, useTheme } from '@mui/material';
+import { Box, FormControl, Grid, InputAdornment, ListSubheader, MenuItem, Select, SelectChangeEvent, TextField, Theme, Typography, useTheme } from '@mui/material';
 import { Link, useParams } from 'react-router-dom';
 import { IIssue } from '../../interfaces/issue.interface';
 import { IProject } from '../../interfaces/project.interface';
 import { IComment } from '../../interfaces/comment.interface';
 import { CommentsAPI } from '../../apis/CommentsAPI';
+import { IUser } from '../../interfaces/user.interface';
 
 const IssueDetail = () => {
     const appCtx = useContext(AppContext);
@@ -17,6 +19,11 @@ const IssueDetail = () => {
     const [issue, setIssue] = React.useState<IIssue | undefined>(undefined);
     const [project, setProject] = React.useState<IProject | undefined>(undefined);
     const [comments, setComments] = React.useState<IComment[] | undefined>(undefined);
+    const [assignedTo, setAssignedTo] = React.useState<string | undefined>(undefined);
+    const containsText = (text: string, searchText: string): boolean => text.toLowerCase().includes(searchText.toLowerCase());
+    const users: IUser[] = appCtx.state.users;
+    const [userSearchText, setUserSearchText] = React.useState<string>('');
+    const displayedUsers = useMemo(() => users.filter((user: IUser) => containsText(user.username, userSearchText)), [users, userSearchText]);
 
     useEffect(() => {
         setIssue(id ? appCtx.state.issues.find((x: IIssue) => x.id === Number.parseInt(id)) : undefined);
@@ -24,8 +31,9 @@ const IssueDetail = () => {
 
     useEffect(() => {
         setProject(appCtx.state.projects.find((x: IProject) => x.id === issue?.related_project_id));
+        setAssignedTo(issue?.assigned_to);
     }, [issue]);
-    
+
     useEffect(() => {
         if (!issue) {
             return;
@@ -35,6 +43,10 @@ const IssueDetail = () => {
             setComments(res);
         });
     }, [issue]);
+
+    const handleAssigneeChange = (event: SelectChangeEvent<string>): void => {
+        setAssignedTo(event.target.value);
+    };
     
     return (
         <Box>
@@ -63,7 +75,65 @@ const IssueDetail = () => {
                     }} />
                 {`ISSUE-${issue?.id}`}
             </Typography>
-            <div>IssueDetail...</div>
+            <Typography variant="h4" component="h2" gutterBottom>{issue?.issue_summary}</Typography>
+            <Grid container spacing={0}>
+                <Grid item={true} xs={12} md={8}>
+                    Left
+                </Grid>
+                <Grid item={true} xs={12} md={4}>
+                    <Box>
+                        <Typography
+                            variant="h6"
+                            component="h3"
+                            gutterBottom
+                            sx={{
+                                color: theme.palette.common.black
+                            }}>Assignee</Typography>
+                        {assignedTo ? (
+                            <FormControl variant="standard">
+                                <Select
+                                    MenuProps={{ 
+                                        autoFocus: false,
+                                    }}
+                                    sx={{
+                                        minWidth: 'calc(100% + 242px)',
+                                    }}
+                                    value={assignedTo}
+                                    onChange={handleAssigneeChange}
+                                    onClose={() => setUserSearchText('')}
+                                    renderValue={(selected: string) => users.find((x: IUser) => x.username === selected)?.username}>
+                                    <ListSubheader>
+                                        <TextField
+                                            autoFocus
+                                            fullWidth
+                                            size="small"
+                                            placeholder="Type to search..."
+                                            InputProps={{
+                                                startAdornment: (
+                                                    <InputAdornment position="start">
+                                                        <SearchIcon />
+                                                    </InputAdornment>
+                                                )
+                                            }}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setUserSearchText(e.target.value)}
+                                            onKeyDown={(e) => {
+                                                if (e.key !== 'Escape') {
+                                                    // Prevents autoselecting item while typing.
+                                                    e.stopPropagation();
+                                                }
+                                            }} />
+                                    </ListSubheader>
+                                    {displayedUsers.map((user: IUser, index: number) => (
+                                        <MenuItem key={index} value={user.username}>
+                                            {user.username}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        ) : 'No one - Assign yourself'}
+                    </Box>
+                </Grid>
+            </Grid>
         </Box>
     );
 };
